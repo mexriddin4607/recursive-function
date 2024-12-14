75cdf9f00c4d0db9b23be73e9f0f281ea077f165
mexriddin4607 <mexriddinaxmedov63@gamail.com>
2024-12-14 18:02:40 +0500

    bajatildi

---
+++ task04.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void func(int n) {
+    if (n <= 0) {
+        return; 
+    }
+    func(n - 1);
+    if (n % 2 != 0) {
+        printf("%d ", n); 
+    }
+}
+
+int main() {
+    int n;
+
+    printf("N ni kiriting: ");
+    scanf("%d", &n);
+
+    printf("Toq sonlar : ");
+    func(n);
+    printf("\n");
+
+    return 0;
+}
